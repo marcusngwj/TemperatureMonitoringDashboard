4dba93012d6bfad4c06b23ce1ee959da616cb777
marcusngwj <marcusngwj@msn.com>
2019-03-17 02:04:00 +0800

    initial commit for floorplan svg

---
+++ imports/ui/components/Floorplan.jsx
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import '../styles/floorplan.scss';
+
+/**
+ * SVG was draw using draw.io: https://www.draw.io/
+ * SVG was minified using SVG Minifier: https://www.svgminify.com/
+ * SVG was beautified using XML Beautifier: http://xmlbeautifier.com/
+ */
+
+export default class Floorplan extends Component {
+  handleClickRoom0 = () => {
+    console.log("Room 0 was clicked");
+  }
+  
+  handleClickRoom1 = () => {
+    console.log("Room 1 was clicked");
+  }
+  
+  handleClickRoom2 = () => {
+    console.log("Room 2 was clicked");
+  }
+
+  handleClickRoom3 = () => {
+    console.log("Room 3 was clicked");
+  }
+
+  handleClickRoom4 = () => {
+    console.log("Room 4 was clicked");
+  }
+
+  handleClickRoom5 = () => {
+    console.log("Room 5 was clicked");
+  }
+
+  handleClickRoom6 = () => {
+    console.log("Room 6 was clicked");
+  }
+
+  render() {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="1247" height="882" viewBox="-0.5 -0.5 1247 882">
+        <path d="M10.5 0h379v300h-379z" className="B F G" onClick={this.handleClickRoom0} />
+        <text x="42" y="26" transform="translate(156.5 128.5)" className="H I J K" onClick={this.handleClickRoom0}>UPPER LEVEL RESIDENTIAL LOUNGE</text>
+        
+        <path d="M10 530h180v340H10z" className="B F G" onClick={this.handleClickRoom1}/>
+        <text x="31" y="19" transform="translate(67.5 685.5)" className="H I J K" onClick={this.handleClickRoom1}>SINGLE BEDROOM</text>
+
+        <path d="M200 530h180v340H200z" className="B F G" onClick={this.handleClickRoom2} />
+        <text x="31" y="19" transform="translate(257.5 685.5)" className="H I J K" onClick={this.handleClickRoom2}>SINGLE BEDROOM</text>
+
+        <path d="M390 530h180v340H390z" className="B F G" onClick={this.handleClickRoom3} />
+        <text x="31" y="19" transform="translate(447.5 685.5)" className="H I J K" onClick={this.handleClickRoom3}>SINGLE BEDROOM</text>
+
+        <path d="M580 530h180v340H580z" className="B F G" onClick={this.handleClickRoom4} />
+        <text x="31" y="19" transform="translate(637.5 685.5)" className="H I J K" onClick={this.handleClickRoom4}>SINGLE BEDROOM</text>
+
+        <path d="M770 530h180v340H770z" className="B F G" onClick={this.handleClickRoom5} />
+        <text x="31" y="19" transform="translate(827.5 685.5)" className="H I J K" onClick={this.handleClickRoom5}>SINGLE BEDROOM</text>
+
+        <path d="M960 530h180v340H960z" className="B F G" onClick={this.handleClickRoom6} />
+        <text x="31" y="19" transform="translate(1017.5 685.5)" className="H I J K" onClick={this.handleClickRoom6}>SINGLE BEDROOM</text>
+        
+        <g className="L B">
+          <path d="M950 880V520h21v10h-11v350z" className="C" />
+          <path d="M1051 520h-80v5h80z" className="D" />
+          <g className="C">
+            <path d="M1051 525c0 44.18-35.82 80-80 80v-80" className="E" />
+            <path d="M920.75 749.25v-98.5h360v10h-350v88.5z" transform="rotate(90 1100.75 700)" />
+          </g>
+        </g>
+        
+        <g className="L B">
+          <path d="M0 880V520h100v10H10v350z" className="C" />
+          <path d="M100 520h80v5h-80z" className="D" />
+          <g className="C">
+            <path d="M100 525c0 44.18 35.82 80 80 80v-80" className="E" />
+            <path d="M10 710v-20h360v10H20v10z" transform="rotate(90 190 700)" />
+          </g>
+        </g>
+        
+        <g className="L B">
+          <path d="M0 0h400v10H0z" />
+          <path d="M765 480v-850h100v10h-90v840z" transform="rotate(270 815 55)" className="C" />
+          <g transform="rotate(270 434.61 147.25)">
+            <path d="M392.5 102.5h84.23v5H392.5z" className="D" />
+            <path d="M392.5 107.5c0 46.52 37.7 84.23 84.23 84.23V107.5" className="C E" />
+          </g>
+          <path transform="rotate(90 395 240)" d="M345 235h100v10H345z" />
+          <path d="M0 290h400v10H0z" />
+          <g transform="rotate(270 49.82 247.5)">
+            <path d="M8 203h83.64v5H8z" className="D" />
+            <path d="M91.64 208c0 46.2-37.45 83.64-83.64 83.64V208" className="C E" />
+          </g>
+          <path transform="rotate(90 5 105)" d="M-95 100h200v10H-95z" />
+          <path d="M43 388h94v5H43z" className="D" />
+          <path d="M90 388v5m0 0c0 25.96-21.04 47-47 47v-47m47 0c0 25.96 21.04 47 47 47v-47" className="C E" />
+          <path d="M260 388h94v5h-94z" className="D" />
+          <path d="M307 388v5m0 0c0 25.96-21.04 47-47 47v-47m47 0c0 25.96 21.04 47 47 47v-47" className="C E" />
+          <path d="M137 388h123v10H137zM0 388h42.5v10H0z" />
+          <path d="M400 300v98h-45.5v-10H390v-88z" className="C" />
+          <path transform="rotate(90 200 343)" d="M150 338h100v10H150z" />
+          <path transform="rotate(90 5 345)" d="M-45 340H55v10H-45z" />
+        </g>
+        
+        <g className="L B">
+          <path d="M190 880V520h100v10h-90v350z" className="C" />
+          <path d="M290 520h80v5h-80z" className="D" />
+          <g className="C">
+            <path d="M290 525c0 44.18 35.82 80 80 80v-80" className="E" />
+            <path d="M200 710v-20h360v10H210v10z" transform="rotate(90 380 700)" />
+            <path d="M380 880V520h21v10h-11v350z" />
+          </g>
+          <path d="M481 520h-80v5h80z" className="D" />
+          <g className="C">
+            <path d="M481 525c0 44.18-35.82 80-80 80v-80" className="E" />
+            <path d="M350.75 749.25v-98.5h360v10h-350v88.5z" transform="rotate(90 530.75 700)" />
+          </g>
+        </g>
+        
+        <g className="L B">
+          <path d="M570 880V520h100v10h-90v350z" className="C" />
+          <path d="M670 520h80v5h-80z" className="D" />
+          <g className="C">
+            <path d="M670 525c0 44.18 35.82 80 80 80v-80" className="E" />
+            <path d="M580 710v-20h360v10H590v10z" transform="rotate(90 760 700)" />
+          </g>
+        </g>
+        
+        <g className="L B">
+          <path d="M760 880V520h100v10h-90v350z" className="C" />
+          <path d="M860 520h80v5h-80z" className="D" />
+          <g className="C">
+            <path d="M860 525c0 44.18 35.82 80 80 80v-80" className="E" />
+            <path d="M770 710v-20h360v10H780v10z" transform="rotate(90 950 700)" />
+          </g>
+          <path d="M10 870h1230v10H10z" />
+          <path transform="rotate(90 630 192.5)" d="M542.5 187.5h175v10h-175z" />
+          <path d="M770 388h475v10H770z" />
+        </g>
+      </svg>
+    );
+  }
+}
--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
+import Floorplan from '../components/Floorplan.jsx';
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     <Hello />
     <Info />
+    <Floorplan />
   </div>
 );
 
