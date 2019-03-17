7a6c3816d1de69e4661e532b0ac6952467cb87a8
marcusngwj <marcusngwj@msn.com>
2019-03-17 09:51:37 +0800

    refactor floorplan

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
-import Floorplan from '../components/Floorplan.jsx';
+import FloorplanView from '../components/FloorplanView.jsx';
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     <Hello />
     <Info />
-    <Floorplan />
+    <FloorplanView />
   </div>
 );
 
