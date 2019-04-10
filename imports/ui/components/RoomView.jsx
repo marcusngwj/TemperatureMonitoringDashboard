41b7659924d93d708ae45fb6f5ba3c6cc5db0caa
LAPTOP-DCAOCMQK\limwe <limweijie1994@gmail.com>
2019-04-10 23:50:17 +0800

    Some view update

--- imports/ui/components/RoomView.jsx
+++ imports/ui/components/RoomView.jsx
@@ -1,16 +1,46 @@
-import React, { Component } from 'react';
-import '../styles/room.scss';
+import React, { Component } from "react";
+import "../styles/room.scss";
 
 export default class RoomView extends Component {
-  handleOnClick = () => {
-    console.log(this.props.id);
-  }
+  constructor(props, defaultColor) {
+    super(props);
+    this.state = { isToggleOn: true };
 
+    this.defaultColor = "red";
+    this.handleOnClick = this.handleOnClick.bind(this);
+    // this.setRoomColor = this.setRoomColor.bind(this);
+    //this.setRoomVisiblity = this.setRoomVisiblity.bind(this);
+  }
+  handleOnClick = () => {
+    //Sends Notification to Presenter to 'hide' the graph
+    console.log("state is " + this.state.roomColor);
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn,
+      roomColor: "white"
+    }));
+  };
+  // setRoomColor = newRoomColor => {
+  //   this.setState(state => ({
+  //     isToggleOn: !state.isToggleOn,
+  //     roomColor: newRoomColor
+  //   }));
+  // };
+  //setRoomVisiblity = () => {};
   render() {
     return (
       <g onClick={this.handleOnClick} className="room">
-        <path d={this.props.d} className="room-background"  />
-        <text x={this.props.x} y={this.props.y} transform={this.props.transform} className="room-text">{this.props.text}</text>
+        {this.state.isToggleOn ? "ON" : "OFF"}
+        <path d={this.props.d} fill={this.state.roomColor} />
+
+        {this.props.setRoomVisibility(this.props.id, this.state.isToggleOn)}
+        <text
+          x={this.props.x}
+          y={this.props.y}
+          transform={this.props.transform}
+          className="room-text"
+        >
+          {this.props.text}
+        </text>
       </g>
     );
   }
