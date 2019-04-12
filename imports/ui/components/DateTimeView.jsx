89ee7fa2ff370c5dfdc96d662bdb87f654e619cc
marcusngwj <marcusngwj@msn.com>
2019-04-12 19:04:00 +0800

    add calendar format date time

--- imports/ui/components/DateTimeView.jsx
+++ imports/ui/components/DateTimeView.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import { DatetimePickerTrigger } from 'rc-datetime-picker';
 import '../styles/datetimeview.scss';
+import 'rc-datetime-picker/dist/picker.css';
 
+/**
+ * https://allenwooooo.github.io/rc-datetime-picker/
+ */
 export default class DateTimeView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      moment: moment()
+    };
+  }
+
+  handleChange = (moment) => {
+    this.setState({
+      moment
+    });
+    console.log(moment);
+  }
+
   render() {
     return (
       <div className={this.props.className}>
-        <div className="datetimeview-main">
-          <span>{this.props.label}</span>
-          <input type="text" />
-          <input type="text" />
-        </div>
+        <DatetimePickerTrigger className="datetimeview-main"
+          moment={this.state.moment}
+          onChange={this.handleChange}>
+          <div className="datetimeview-background">
+            <span>{this.props.label}</span>
+            <input value={this.state.moment.format('YYYY-MM-DD HH:mm')} className="datetimeview-textvalue-area" type="text" readOnly />
+          </div>
+        </DatetimePickerTrigger>
       </div>
     );
   }
